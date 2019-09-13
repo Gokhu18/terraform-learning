3855729ee31ebc42c7000c56f9d23be469a6342a
Quentin Lemaire <quentin.lemaire10@free.fr>
2019-09-13 11:26:09 +0200

    Getting started GCP

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "service_account_key" {
+  type = "string"
+  description = "Service account JSON file to access Google Cloud API"
+}
+
+variable "project" {
+  type = "string"
+  description = "Google Cloud project name"
+}
